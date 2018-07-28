0a24e32f61f08832f60963b20f067a11bfc8d6fc
UstymUkhman <ustym.ukhman@gmail.com>
2018-07-28 15:52:16 +0200

    Mouse position mask added

--- src/glsl/transition.frag
+++ src/glsl/transition.frag
@@ -4,29 +4,19 @@ uniform sampler2D firstTexture;
 uniform sampler2D secondTexture;
 uniform sampler2D displacement;
 
-uniform float transitionTimer;
+uniform vec2  mousePosition;
+uniform float maxRadius;
+uniform float minRadius;
 
-// varying vec3 vVertexPosition;
 varying vec2 vTextureCoord;
 
 
 void main (void) {
-  vec2 textureCoords = vec2(vTextureCoord.x, vTextureCoord.y);
-  // vec4 finalColor = texture2D(firstTexture, textureCoords);
-
-  // finalColor.rgb -= clamp(-vVertexPosition.z, 0.0, 1.0);
-  // finalColor.rgb += clamp(vVertexPosition.z, 0.0, 1.0);
-
-  vec4 displacementTexture = texture2D(displacement, textureCoords);
-  float displacementFactor = (cos(transitionTimer / (90.0 / 3.141592)) + 1.0) / 2.0;
-
-  vec2 firstDisplacementCoords = vec2(textureCoords.x + displacementFactor * (displacementTexture.r * 1.0), textureCoords.y);
-  vec2 secondDisplacementCoords = vec2(textureCoords.x - (1.0 - displacementFactor) * (displacementTexture.r * 1.0), textureCoords.y);
-
-  vec4 firstDistortedColor = texture2D(firstTexture, firstDisplacementCoords);
-  vec4 secondDistortedColor = texture2D(secondTexture, secondDisplacementCoords);
-  vec4 finalColor = mix(firstDistortedColor, secondDistortedColor, displacementFactor);
-
-  finalColor = vec4(finalColor.rgb * finalColor.a, finalColor.a);
-  gl_FragColor = finalColor;
+  vec4 color0 = texture2D(firstTexture, vTextureCoord);
+  vec4 color1 = texture2D(secondTexture, vTextureCoord);
+  
+  float dist = distance(mousePosition, gl_FragCoord.xy);
+  float mixAmount = clamp((dist - minRadius) / (maxRadius - minRadius), 0.0, 1.0);
+  
+  gl_FragColor = mix(color0, color1, mixAmount);
 }
--- src/index.js
+++ src/index.js
@@ -4,30 +4,25 @@ export default class VideoTransition {
     this.pixelRatio = window.devicePixelRatio || 1.0;
 
     // this.mouseLastPosition = { x: 0, y: 0 };
-    // this.mousePosition = { x: 0, y: 0 };
+    this.mousePosition = { x: 0, y: 0 };
     // this.mouseDelta = 0;
+
+    this.maxRadius = 300;
+    this.minRadius = 100;
+    this.radius = 120;
 
     this.secondVideoReady = false;
     this.firstVideoReady = false;
     this.firstVideoReady = false;
 
-    this.transitionTimer = 0;
+    // this.transitionTimer = 0;
     this.activeTexture = 1;
 
     this.plane = new Curtains('webgl').addPlane(this.planeElement, {
       fragmentShader: require('./glsl/transition.frag'),
       vertexShader: require('./glsl/transition.vert'),
 
-      // heightSegments: 20,
-      // widthSegments: 20,
-
-      /* uniforms: {
-        mouseMoveStrength: {
-          name: 'movementStrength',
-          type: '1f',
-          value: 0
-        },
-
+      uniforms: {
         mousePosition: {
           name: 'mousePosition',
           type: '2f',
@@ -37,51 +32,49 @@ export default class VideoTransition {
           ]
         },
 
-        resolution: {
-          name: 'resolution',
-          type: '2f',
-          value: [
-            this.pixelRatio * this.planeElement.clientWidth,
-            this.pixelRatio * this.planeElement.clientHeight
-          ]
+        maxRadius: {
+          value: this.maxRadius,
+          name: 'maxRadius',
+          type: '1f'
         },
 
-        time: {
-          name: 'time',
-          type: '1f',
-          value: 0
+        minRadius: {
+          value: this.minRadius,
+          name: 'minRadius',
+          type: '1f'
         }
-      } */
 
-      uniforms: {
-        resolution: {
-          name: 'resolution',
-          type: '2f',
-          value: [
-            this.pixelRatio * this.planeElement.clientWidth,
-            this.pixelRatio * this.planeElement.clientHeight
-          ]
-        },
+        // transitionTimer: {
+        //   name: 'transitionTimer',
+        //   type: '1f',
+        //   value: 0,
+        // },
 
-        transitionTimer: {
-          name: 'transitionTimer',
-          type: '1f',
-          value: 0,
-        },
+        // resolution: {
+        //   name: 'resolution',
+        //   type: '2f',
+        //   value: [
+        //     this.pixelRatio * this.planeElement.clientWidth,
+        //     this.pixelRatio * this.planeElement.clientHeight
+        //   ]
+        // },
+
+        // time: {
+        //   name: 'time',
+        //   type: '1f',
+        //   value: 0
+        // }
       }
     });
 
     this.plane.onReady(this.onPlaneReady.bind(this)).onRender(this.onPlaneRender.bind(this));
-
     document.getElementById('video-1').addEventListener('canplay', this.onReady.bind(this));
     document.getElementById('video-2').addEventListener('canplay', this.onReady.bind(this));
   }
 
   onPlaneReady () {
-    // this.planeElement.addEventListener('mousemove', this.onMouseMove.bind(this));
-    // this.plane.setPerspective(35);
-
-    this.planeElement.addEventListener('click', this.onClick.bind(this));
+    this.plane.setPerspective(35);
+    this.planeElement.addEventListener('mousemove', this.onMouseMove.bind(this));
   }
 
   onReady (event) {
@@ -99,44 +92,37 @@ export default class VideoTransition {
   }
 
   onPlaneRender () {
-    // this.plane.uniforms.time.value++;
     // this.plane.uniforms.mouseMoveStrength.value = this.mouseDelta;
     // this.mouseDelta = Math.max(0, this.mouseDelta * 0.995);
 
-    if(this.activeTexture === 1) {
-      this.transitionTimer = Math.max(0, this.transitionTimer - 1);
-    } else {
-      this.transitionTimer = Math.min(90, this.transitionTimer + 1);
-    }
+    // if(this.activeTexture === 1) {
+    //   this.transitionTimer = Math.max(0, this.transitionTimer - 1);
+    // } else {
+    //   this.transitionTimer = Math.min(90, this.transitionTimer + 1);
+    // }
 
-    this.plane.uniforms.transitionTimer.value = this.transitionTimer;
+    // this.plane.uniforms.transitionTimer.value = this.transitionTimer;
   }
 
-  /* onMouseMove (event) {
-    this.mouseLastPosition.x = this.mousePosition.x;
-    this.mouseLastPosition.y = this.mousePosition.y;
+  onMouseMove (event) {
+    // this.mouseLastPosition.x = this.mousePosition.x;
+    // this.mouseLastPosition.y = this.mousePosition.y;
 
     this.mousePosition.x = event.clientX;
-    this.mousePosition.y = event.clientY;
+    this.mousePosition.y = this.planeElement.clientHeight + this.radius - event.clientY;
+    this.plane.uniforms.mousePosition.value = [this.mousePosition.x, this.mousePosition.y];
 
-    const mouseCoords = this.plane.mouseToPlaneCoords(this.mousePosition.x, this.mousePosition.y);
-    this.plane.uniforms.mousePosition.value = [mouseCoords.x, mouseCoords.y];
+    // if (this.mouseLastPosition.x && this.mouseLastPosition.y) {
+    //   const delta = Math.min(4, Math.sqrt(
+    //     Math.pow(this.mousePosition.x - this.mouseLastPosition.x, 2) +
+    //     Math.pow(this.mousePosition.y - this.mouseLastPosition.y, 2)
+    //   ) / 30);
 
-    if (this.mouseLastPosition.x && this.mouseLastPosition.y) {
-      const delta = Math.min(4, Math.sqrt(
-        Math.pow(this.mousePosition.x - this.mouseLastPosition.x, 2) +
-        Math.pow(this.mousePosition.y - this.mouseLastPosition.y, 2)
-      ) / 30);
-
-      if (delta >= this.mouseDelta) {
-        this.mouseDelta = delta;
-        this.plane.uniforms.time.value = 0;
-      }
-    }
-  } */
-
-  onClick (event) {
-    this.activeTexture = this.activeTexture === 1 ? 2 : 1;
+    //   if (delta >= this.mouseDelta) {
+    //     this.mouseDelta = delta;
+    //     this.plane.uniforms.time.value = 0;
+    //   }
+    // }
   }
 
   onResize () {
